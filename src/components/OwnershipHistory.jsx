e976cd9336b70b90145a714a9e137edc959d9dce
Syed Sharukhali Farookali Naziyabegum <99337487+AI-ApeX-DeV@users.noreply.github.com>
2024-04-12 10:30:02 +0530

    feat: added ownedbyyou,ownership,profile

---
+++ src/components/OwnershipHistory.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const OwnershipHistory = () => {
+  return (
+    
+      
+
+      <div className="">
+       <img className='w-[648px] h-auto object-cover' src="https://i.imgur.com/7wSo3VD.jpeg" alt="" />
+    </div>
+  )
+}
+
+export default OwnershipHistory
